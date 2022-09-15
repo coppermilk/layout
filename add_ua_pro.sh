dac87e655f3fa2db335b7577916e29d000d9f870
Mr. Coppermilk <nataliyakowtun67@gmail.com>
2022-09-16 00:31:19 +0200

    Update add_ua_pro.sh

--- add_ua_pro.sh
+++ add_ua_pro.sh
@@ -1,3 +1,3 @@
 wget -P /usr/share/X11/xkb/symbols/ https://raw.githubusercontent.com/coppermilk/layout/main/linux/uadiktordworak
 sed -i'' '/! layout/a uadiktordworak    Ukrainian Diktor + Dvorak' /usr/share/X11/xkb/rules/evdev.lst /usr/share/X11/xkb/rules/base.lst
-sed -i'' '/<layoutList\>/a <layout><configItem><name>ua pro</name><shortDescription>ua</shortDescription><description>ua pro</description><languageList><iso639Id>uk</iso639Id></languageList></configItem><variantList/></layout>' /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/base.xml
+sed -i'' '/<layoutList\>/a <layout><configItem><name>ua pro</name><shortDescription>ua</shortDescription><description>ua pro</description><languageList><iso639Id>ukr</iso639Id></languageList></configItem><variantList/></layout>' /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/base.xml
