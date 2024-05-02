4f0e7efd0d3f21f232dbaa0ad5a1c3ad25fd9450
Artem Herych <nataliyakowtun67@gmail.com>
2024-05-02 18:47:49 +0200

    Create add_diktor.sh

---
+++ linux/add_diktor.sh
@@ -0,0 +1,3 @@
+wget -P /usr/share/X11/xkb/symbols/ https://raw.githubusercontent.com/coppermilk/layout/master/linux/diktor
+sed -i'' '/! layout/a diktor    Russian Typewriter Diktor' /usr/share/X11/xkb/rules/evdev.lst /usr/share/X11/xkb/rules/base.lst
+sed -i'' '/<layoutList\>/a <layout><configItem><name>diktor</name><shortDescription>diktor</shortDescription><description>Typewriter Russian Diktor</description><languageList><iso639Id>rus</iso639Id></languageList></configItem><variantList/></layout>' /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/base.xml
