df33465c94dd24cf0e22de7ea55f2c200c3975ba
Artem Herych <nataliyakowtun67@gmail.com>
2024-05-05 17:28:20 +0200

    Update add_diktor_ua.sh

--- linux/add_diktor_ua.sh
+++ linux/add_diktor_ua.sh
@@ -1,3 +1,3 @@
-wget -P /usr/share/X11/xkb/symbols/ https://raw.githubusercontent.com/coppermilk/layout/main/linux/diktorua
-sed -i'' '/! layout/a diktor    Ukranian Typewriter Diktor' /usr/share/X11/xkb/rules/evdev.lst /usr/share/X11/xkb/rules/base.lst
-sed -i'' '/<layoutList\>/a <layout><configItem><name>diktor</name><shortDescription>diktor</shortDescription><description>Typewriter Ukranian Diktor</description><languageList><iso639Id>rus</iso639Id></languageList></configItem><variantList/></layout>' /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/base.xml
+wget -P /usr/share/X11/xkb/symbols/ https://raw.githubusercontent.com/coppermilk/layout/master/linux/diktoruaua
+sed -i'' '/! layout/a diktorua    Ukranian Typewriter Diktor' /usr/share/X11/xkb/rules/evdev.lst /usr/share/X11/xkb/rules/base.lst
+sed -i'' '/<layoutList\>/a <layout><configItem><name>diktorua</name><shortDescription>diktorua</shortDescription><description>Typewriter Ukranian Diktor</description><languageList><iso639Id>rus</iso639Id></languageList></configItem><variantList/></layout>' /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/base.xml
